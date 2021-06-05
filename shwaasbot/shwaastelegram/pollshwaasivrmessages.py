a5b20314384320dac5eba4d1d8b424b760684589
Arjun Venkatraman <mojoarjun@gmail.com>
2021-06-05 21:53:12 +0530

    Updated

--- shwaasbot/shwaastelegram/pollshwaasivrmessages.py
+++ shwaasbot/shwaastelegram/pollshwaasivrmessages.py
@@ -1,20 +1,23 @@
 
 import os
 from shwaastg.shwaastgbot import *
-
+import time
 
 if __name__ == "__main__":
     while True:
-        call_list = os.listdir("/opt/voiceofhimalaya/calls")
-        call_list.remove("calllog")
-        for call in call_list:
-            files = os.listdir(os.path.join("/opt/voiceofhimalaya/calls", call))
-            if "telegramed" in files:
-                continue
-            for fn in files:
-                if ".wav" in fn:
-                    shwaastgbot.updater.bot.send_audio(-456601244, open(
-                        os.path.join("/opt/voiceofhimalaya/calls", call, fn), "rb"), caption=call)
-                    with open(os.path.join(os.path.join("/opt/voiceofhimalaya/calls", call, "telegramed")), "w") as f:
-                        f.write("Telegramed")
-        xpal.shwaastgbotxpal.wait()
+        try:
+            call_list = os.listdir("/opt/voiceofhimalaya/calls")
+            call_list.remove("calllog")
+            for call in call_list:
+                files = os.listdir(os.path.join("/opt/voiceofhimalaya/calls", call))
+                if "telegramed" in files:
+                    continue
+                for fn in files:
+                    if ".wav" in fn:
+                        shwaastgbot.updater.bot.send_audio(-456601244, open(
+                            os.path.join("/opt/voiceofhimalaya/calls", call, fn), "rb"), caption=call)
+                        with open(os.path.join(os.path.join("/opt/voiceofhimalaya/calls", call, "telegramed")), "w") as f:
+                            f.write("Telegramed")
+        except Exception as e:
+                print("{} {}".format(str(e), repr(e)))
+        time.sleep(600)
